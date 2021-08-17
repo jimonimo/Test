6859e06673f74b6a08a3d802c0ae904c6462d430
jimonimo <ljia0597@gmail.com>
2021-08-17 13:22:11 +0900

    본석님 안되는 거 -이미지 교체 테스트

---
+++ AProject/Assets/Scripts/ClickImaged.cs
@@ -0,0 +1,27 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickImaged : MonoBehaviour
+{
+    public Image[] images = new Image[2];
+    public bool isClick;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+
+    void Start()
+    {
+        isClick = false;
+        //spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeImage()
+    {
+        ChangeSprite();
+    }
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = newSprite;
+    }
+}
